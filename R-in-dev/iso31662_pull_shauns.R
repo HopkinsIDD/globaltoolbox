0d6c44aa53c428e4f0f806d36a570fa81ce9f189
shauntruelove <satruelove@gmail.com>
2019-09-11 16:21:29 -0400

    New version. probably can replace the other as this works. Annoyingly, it requires input of chrome version.

---
+++ R-in-dev/iso31662_pull_shauns.R
@@ -0,0 +1,91 @@
+get_iso_country_codes <- function(country, port=4567L, chromever = "latest"){
+  
+  rm(driver)
+  gc() 
+  
+  if(length(country) > 1){
+    stop("country should be a single country")
+  }
+  if(mode(country) != 'character'){
+    country = as.character(country)
+  }
+  if(nchar(country) != 2){
+    stop("country should be the iso alpha-2 code")
+  }
+  weburl = paste0("https://www.iso.org/obp/ui/#iso:code:3166:",country)
+
+  eCaps <- list(chromeOptions = list(args = c('--no-sandbox','--headless', '--disable-gpu', '--window-size=1280,800')))
+  driver <- RSelenium::rsDriver(browser=browser, port=port, chromever = chromever, extraCapabilities=eCaps)
+  Sys.sleep(1) # Sleep to give it a chance to work
+  remDr <- driver[["client"]]
+  Sys.sleep(1)
+  
+  #Access Chrome driver
+  #RSelenium::startServer()
+  #startServer(javaargs="/users/name/folder/chromedriver") #path to where chromedriver is located on local hard (downloaded from: https://sites.google.com/a/chromium.org/chromedriver/downloads)
+  #remDr <- remoteDriver(browserName = "chrome") 
+
+  remDr$open()
+  Sys.sleep(1)
+  
+  if(!remDr$getStatus()$ready){
+    stop("The web browser did not initialize successfully")
+  }
+
+  remDr$navigate(url = weburl)
+  Sys.sleep(1)
+  webElem <- remDr$findElement(using = "xpath", '//*[(@id = "subdivision")]//td')
+  Sys.sleep(1)
+  websrc <- webElem$getPageSource()[[1]] %>%
+    xml2::read_html()
+  # rc <- websrc %>% 
+  #   xml2::xml_children() %>%
+  #   .[[2]] %>%
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[2]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[2]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>%
+  #   xml2::xml_children() %>%
+  #   .[[2]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[2]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[2]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[1]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[5]] %>% 
+  #   xml2::xml_children() %>%
+  #   .[[7]]  
+    
+  rc <- websrc %>% rvest::html_table()
+  rc <- rc[[3]] 
+  remDr$close()
+  rm(driver) # make sure port is not in use next time
+  gc()
+  return(rc)
+}
